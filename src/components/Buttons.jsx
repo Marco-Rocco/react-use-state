b27850d1bc86b61b51f33cb8e1da06bbd1849a58
Marco-Rocco <marcorocco28@icloud.com>
2025-04-08 16:56:14 +0200

    feat: create card components

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,12 +1,10 @@
-// import languages from "../assets/languages"
-
 export default function buttons(props) {
     const languages = props.languages;
     return (
         <>
             {
                 languages.map(element =>
-                    <button key={element.id}>{element.title}</button>
+                    <button key={element.id} onClick={() => setDetails(element)} >{element.title}</button>
                 )
             }
         </>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import languages from "../assets/languages"
 import Buttons from "./Buttons"
+import Card from "./Card"
 
 export default function Main() {
     return (
         <>
             <main>
                 <Buttons languages={languages} />
+                <Card />
             </main>
         </>
     )
